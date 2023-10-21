9c44319f99e86f0f1cc79fa42645e598fbe9533d
mdeme01 <se4n01@gmail.com>
2023-10-21 12:43:42 +0200

    feat: docker postgres db container, prisma client, create user route

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import express from 'express';
 
@@ -5,11 +7,44 @@ dotenv.config();
 
 const server_port = Number(process.env['SERVER_PORT'] || 8080);
 
+const prisma = new PrismaClient();
+
 const main = async (): Promise<void> => {
   const app = express();
 
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
   app.get('/', (_req, res) => {
     res.status(200).send({ data: 'Hello world!' });
+  });
+
+  app.post('/user/create', async (req, res) => {
+    const { name, email, password } = req.body as {
+      name: string;
+      email: string;
+      password: string;
+    };
+
+    try {
+      const password_hash = await bcrypt.hash(password, 10);
+
+      await prisma.users.create({
+        data: {
+          name,
+          email,
+          password_hash,
+        },
+      });
+
+      res.status(201).send({
+        message: 'User created.',
+      });
+    } catch {
+      res.status(400).send({
+        message: 'Error creating user.',
+      });
+    }
   });
 
   app.listen(server_port, () =>
@@ -18,5 +53,11 @@ const main = async (): Promise<void> => {
 };
 
 main()
-  .then()
-  .catch((e) => console.error(e));
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
